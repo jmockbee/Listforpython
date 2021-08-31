a15226ea99d039bbe3348a5658fe34aad31b369a
Jon Mockbee <mockbee1000@gmail.com>
2021-08-31 19:01:32 -0400

    a tuple exampe

---
+++ example5.py
@@ -0,0 +1,8 @@
+def file_size(file_info):
+    ___, ___, ___ = file_info
+    return "{:.2f}".format(___ / 1024)
+
+
+print(file_size(("Class Assignment", "docx", 17875)))  # Should print 17.46
+print(file_size(("Notes", "txt", 496)))  # Should print 0.48
+print(file_size(("Program", "py", 1239)))  # Should print 1.21
