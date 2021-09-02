0d706ae851dbb239bc248ea35605a94da9bc537f
Jon Mockbee <mockbee1000@gmail.com>
2021-09-01 20:14:16 -0400

    next example

---
+++ example8.py
@@ -0,0 +1,12 @@
+def skip_elements(elements):
+    # code goes here
+
+    return ___
+
+
+print(
+    skip_elements(["a", "b", "c", "d", "e", "f", "g"])
+)  # Should be ['a', 'c', 'e', 'g']
+print(
+    skip_elements(["Orange", "Pineapple", "Strawberry", "Kiwi", "Peach"])
+)  # Should be ['Orange', 'Strawberry', 'Peach']
