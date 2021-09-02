7ea19b37d5e97e81f997fcdbcbe2d1121f874837
Jon Mockbee <mockbee1000@gmail.com>
2021-09-02 19:51:36 -0400

    trying to get multiples of 3 after the range

---
+++ example11.py
@@ -0,0 +1,6 @@
+languages = ["python", "perl", "ruby", "go", "java", "c"]
+lengths = [len(language) for language in languages]
+print(lengths)
+
+z = [x for x in range(0, 101) if x % 3 == 0]
+print(z)
